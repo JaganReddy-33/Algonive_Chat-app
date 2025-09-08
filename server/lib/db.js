8ed79d142321a927a6c9858c137ce37794be5a2f
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2025-09-08 12:46:11 +0530

    Updated MongoDB connection settings in db.js

--- server/lib/db.js
+++ server/lib/db.js
@@ -6,7 +6,7 @@ export const connectDB = async () => {
       console.log("✅ MongoDB connected");
     });
 
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(`${process.env.MONGODB_URI}/ChatApp`);
 
   } catch (error) {
     console.error("❌ Failed to connect:", error);
